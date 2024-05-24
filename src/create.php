250e4796e323710ea5323505cbac98b2107e35fc
osias-espindola <167890686+osias-espindola@users.noreply.github.com>
2024-05-24 14:57:43 +0200

    correction  $query

--- src/create.php
+++ src/create.php
@@ -7,7 +7,7 @@ $sql = "INSERT INTO users (first_name, last_name)
         VALUES (:first_name, last_name)";
         
 $query->prepare($sql);
-$query->bindevalue(":first_name , $first_name);
-$query->bindevalue(":last_name , $last_name);
+$query->bindevalue(":first_name", $first_name);
+$query->bindevalue(":last_name", $last_name);
 
 $query->execute();

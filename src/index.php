c172ed0a23482f8659da13d57ebcdf1e3f7705a9
osias-espindola <167890686+osias-espindola@users.noreply.github.com>
2024-05-24 11:06:25 +0200

    activite atelier_crud en cours

---
+++ src/index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Atelier CRUD</title>
+</head>
+<body>
+    <h1>Liste des Ultilisatuer</h1>
+    <table>
+        <thead>
+            <td>id</td>
+            <td>prenome</td>
+            <td>nom</td>
+        </thead>
+    </table>
+    <tbody>
+        <tr>
+            <td>0</td>
+            <td>Luiza</td>
+            <td>Maria</td>
+        </tr>
+    </tbody>
+    
+</body>
+</html>

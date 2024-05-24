27b97d599cb052666e73b46a5c162c8d1dbee0e8
osias-espindola <167890686+osias-espindola@users.noreply.github.com>
2024-05-24 14:42:57 +0200

    file create

---
+++ src/create.php
@@ -0,0 +1,10 @@
+<?php //porqua on ne ferme pas le php ?
+$first_name = $_POSTE["first_name"];
+$first_name = $_POSTE["last_name"];
+
+$sql = "INSERT INTO users (first_name, last_name)
+        VALUES (:first_name, last_name)";
+        
+$query->prepare($sql);
+$query->bindevalue(":first_name , $first_name");
+$query->bindevalue(":last_name , $last_name");

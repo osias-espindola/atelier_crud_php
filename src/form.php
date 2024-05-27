804cb0a62e0230ad6057c3bccde27035561d65e8
osias-espindola <167890686+osias-espindola@users.noreply.github.com>
2024-05-27 10:09:47 +0200

    correction form.php

--- src/form.php
+++ src/form.php
@@ -5,16 +5,19 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Formulaire</title>
 </head>
-<body>
-    <h1>Ajout d'un utilisateur</h1>
-    <form action="create.php" method="post">
-        <label for="first_name">Prénom</label>
-        <input type="text"name="first_name" required >
-        <label for="last_name">Nom</label>
-        <input type="text"name="last_name" required>
-        <button>Ajouter</button>
+<body><h1>Ajouter un.e utilisateurice</h1>
+<form action="create.php" method="post">  
+    <label for="first_name">
+        Prénom
+    </label>
+    <input type="text" name='first_name' required>
+    <label for="last_name">
+        Nom
+    </label>
+    <input type="text" name='last_name' required>
+    <button>Ajouter</button>
     </form>
-    <a href="index.php">Retour</a>
-
+    <a href="atelier.php">Retour</a> 
+    <?php echo print_r($_POST);?>
 </body>
 </html>

f101875353653f9a50b4993b4f67c720ed52128a
osias-espindola <167890686+osias-espindola@users.noreply.github.com>
2024-05-27 10:07:48 +0200

    correction connect.php

--- src/connect.php
+++ src/connect.php
@@ -1,16 +1,17 @@
 <?php 
-    const DBHOST = "db";
-    const DBNAME = "atelier_crud";
-    const DBUSER = "test";
-    const DBPASSWORD = "test";
-    
-    $dsn = "mysql:host=" . DBHOST . ";dbname=" . DBNAME . ";charset=utf8";
+const DBHOST = 'db';
+const DBNAME = 'atelier_crud';
+const DBUSER = 'test';
+const DBPASS = 'test';
 
-    // Teste para a conexão
-    try {
-        $db = new PDO($dsn, DBUSER, DBPASSWORD);
-        echo "Conexão reussi" . "<br>";
-    } catch(PDOException $error){
-        echo "Échec la connexion: " . $error->getMessage() . "<br>"; 
-    }
-?>
+    //c'est ça qu'il nous manquait ! le ";" veut lui signifier "ça y est c'est fait"
+$dsn = "mysql:host=" . DBHOST . ";dbname=" . DBNAME . ";charset=utf8";
+
+    //Maintenant, on se connecte avec le PDO
+try {
+$db = new PDO($dsn, DBUSER, DBPASS);
+// echo "connexion réussie";
+} catch(PDOException $error) {
+    echo "échec de la connection: " . $error->getMessage() . "</br>";
+}
+//ci-dessus la fonction nous sort le message d'erreur en cas d'échec

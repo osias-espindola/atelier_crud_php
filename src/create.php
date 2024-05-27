0249eb8a3969770c69071b8e6f0c8495c7e12817
osias-espindola <167890686+osias-espindola@users.noreply.github.com>
2024-05-27 08:54:49 +0200

    correction create

--- src/create.php
+++ src/create.php
@@ -1,24 +1,10 @@
-<?php // Não é necessário fechar a tag PHP se o arquivo contém apenas código PHP.
-
-if(
-    isset($_POST["first_name"]) && !empty($_POST["first_name"])
-    && isset($_POST["last_name"]) && !empty($_POST["last_name"])
-    ){
-require_once("connect.php");
-
-$first_name = strip_tags($_POST["first_name"]); // Corrigido para $_POST
-$last_name = strip_tags($_POST["last_name"]); // Corrigido para a variável correta e $_POST
-
-$sql = "INSERT INTO user (first_name, last_name)
-        VALUES (:first_name, :last_name)"; // Adicionado os dois pontos para o placeholder :last_name
-        
-$query = $db->prepare($sql); // Corrigido para usar $db que é o objeto PDO
-$query->bindValue(":first_name", $first_name); // Corrigido para bindValue
-$query->bindValue(":last_name", $last_name); // Corrigido para bindValue
-
-$query->execute();
-
-header("Location: index.php");
-} else {
-    echo "Ramplir le formulaire!";
-}
+<?php if(      isset($_POST["first_name"]) && 
+!empty($_POST["first_name"])     && isset($_POST["last_name"]) && 
+!empty($_POST["last_name"])  ){ require_once("connect.php");   
+$first_name = strip_tags($_POST["first_name"]); $last_name = strip_tags($_POST["last_name"]);   
+$sql = "INSERT INTO users (first_name,last_name)        
+VALUES (:first_name,:last_name)";     
+$query = $db->prepare($sql); $query->bindValue(":first_name",$first_name); 
+$query->bindValue(":last_name",$last_name);   
+$query->execute();  header("location: index.php"); } 
+else{     echo "Veuiller remplir le formulaire"; }
